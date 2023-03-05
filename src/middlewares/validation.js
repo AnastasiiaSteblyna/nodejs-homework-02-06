a8dac68c5b6c030f080963b203ec5de7c9874997
AnastasiiaSteblyna <98827282+AnastasiiaSteblyna@users.noreply.github.com>
2023-03-05 13:34:13 +0200

    almost done

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,13 +1,31 @@
-const { boolean } = require("joi");
-const Joi = require("joi");
+module.exports = {
+  contactValidation: (req, res, next) => {
+    const schema = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .required(),
+      phone: Joi.string().min(1).max(30).required(),
+      favorite: Joi.boolean(),
+    });
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: "missing required name field" });
+    }
+    next();
+  },
 
-const schema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().min(1).max(30).required(),
-  favorite: Joi.boolean(),
-});
-
-module.exports = { schema };
+  userValidation: (req, res, next) => {
+    const userSchema = Joi.object({
+      email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+        .required(),
+      password: Joi.string().min(6).max(30).required(),
+    });
+    const validationResult = userSchema.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: "missing required name field" });
+    }
+    next();
+  },
+};
--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
 
-const contactShema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
-    },
-    phone: {
-      type: String,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
+const contactShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
   },
-  { versionKey: false, timestamps: true }
-);
+  email: {
+    type: String,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    unique: true,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "user",
+  },
+});
 
 const Contact = mongoose.model("contact", contactShema);
 
